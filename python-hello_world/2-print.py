c7812c4dfe2cf0c4e66bc744b66e4ecafb9f411d
benjaminparentt <p.benjamin620@icloud.com>
2024-01-25 03:01:38 -0800

    fon,ction print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 # This Python script prints "Programming is like building a multilingual puzzle"
-# followed by a new line.
-
-print("Programming is like building a multilingual puzzle,")
+print("Programming is like building a multilingual puzzle")

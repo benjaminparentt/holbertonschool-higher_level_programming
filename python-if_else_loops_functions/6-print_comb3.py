7f6e8dc06d7c1842b3062a8d16870a9cd17bc80b
benjaminparentt <p.benjamin620@icloud.com>
2024-01-26 05:35:09 -0800

    change 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for dig1 in range(01, 10):
-        for dig2 in range(0, 10):
-                if dig1 < dig2:
-                        if dig1 < 8:
-                                print("{}{}, ".format(dig1, dig2), end="")
-                        else:
-                                print("{}{}".format(dig1, dig2))
+for i in range(0, 8):
+    for j in range(i, 10):
+        if i != j:
+            print("{:d}{:d}, ".format(i, j), end='')
+print("89")

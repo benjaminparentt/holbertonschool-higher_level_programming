5849fd68c31cbf8ef5d720769bdea274c2111432
benjaminparentt <p.benjamin620@icloud.com>
2024-02-02 16:06:59 +0100

    README

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     """
-    Returns a set of common elements in two sets
+    Returns a set of all elements present in only one set
     ...
 
     Parameters
@@ -15,4 +15,4 @@ def common_elements(set_1, set_2):
         the result of the operation (&)
     """
 
-    return (set_1 & set_2)
+    return (set_1 ^ set_2)

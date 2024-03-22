a875cd3408b1881ee4b75202abe2ffdcb4990af8
benjaminparentt <p.benjamin620@icloud.com>
2024-03-22 21:21:24 +0100

    add 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Attempt to list privileges for 'user_0d_1'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Separator line for clarity in output
+SELECT '-------------------------------------------' AS '';
+
+-- Attempt to list privileges for 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
